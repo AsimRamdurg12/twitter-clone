eed7a78412427453f0c40b2e4341eae8276e1e15
AsimRamdurg12 <asimramdurg12@gmail.com>
2024-12-10 09:35:30 +0530

    init

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
+app.use(express.json({ limit: "5mb" })); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
